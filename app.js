d602126f1776627662ec3b427ed15959da146641
iftemplar <samila.mykola@gmail.com>
2023-10-24 23:34:49 +0200

    Configured Routing using Express.js

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ const appRouter = require('./routes/router');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(appRouter);
+app.use('/', (req, res, next) => {
+    res.send('<h1>This should not work if Router is configured</h1>');
+})
 
 app.listen(3000);
--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 router.get('/', (req, res, next) => {
-    res.send('<h1>TEST 1</h1>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'car-list.html'))
 })
 
 router.get('/car-details', (req, res, next) => {
-    res.send('<h1>Car Details</h1>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'car-details.html'))
 })
 
 module.exports = router;
